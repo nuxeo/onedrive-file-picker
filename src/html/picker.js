dd4db5373df8f834a204b914731d03fbf3931082
Kevin Leturc <kleturc@nuxeo.com>
2016-02-22 16:08:25 +0100

    NXP-19019: Update demo style

--- src/html/picker.js
+++ src/html/picker.js
@@ -11,12 +11,12 @@ const picker =
       <div onedrive-insert-breadcrumb></div>
       <div class="odfp-search">
         <input class="odfp-search-input" type="text" />
-        <input class="odfp-search-submit" type="submit" value="Search" />
+        <input class="odfp-search-submit odfp-button" type="submit" value="Search" />
       </div>
       <div onedrive-insert-rows></div>
     </div>
     <div class="odfp-footer">
-      <input class="odfp-select" type="submit" value="Select" />
+      <input class="odfp-select odfp-button" type="submit" value="Select" />
     </div>
   </div>
 </div>`;
