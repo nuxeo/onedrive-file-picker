720a6aed6cf40082399dccf6c4d801a1c846063c
Kevin Leturc <kleturc@nuxeo.com>
2016-02-25 18:19:40 +0100

    NXP-19403: Add a font icon and use them for item without thumbnails

--- src/html/item.js
+++ src/html/item.js
@@ -4,6 +4,7 @@ const item =
 `<div class="odfp-item">
   <div class="odfp-thumbnail">
     <div class="odfp-picture"></div>
+    <div class="odfp-icon"></div>
   </div>
   <div class="odfp-name"></div>
 </div>`;
--- src/item-view.js
+++ src/item-view.js
@@ -24,7 +24,22 @@ class ItemView {
     _item.find('.odfp-name').append(this._itemData.name);
     const thumbnails = this._itemData.thumbnails;
     if (thumbnails && thumbnails.length > 0) {
-      _item.find('.odfp-thumbnail .odfp-picture').attr('style', `background-image: url("${thumbnails[0].medium.url}");`);
+      _item.find('.odfp-thumbnail .odfp-picture')
+        .attr('style', `background-image: url("${thumbnails[0].medium.url}");`);
+      _item.find('.odfp-thumbnail .odfp-icon').hide();
+    } else {
+      _item.find('.odfp-thumbnail .odfp-picture').hide();
+      if (this._itemData.folder) {
+        _item.find('.odfp-thumbnail .odfp-icon').addClass('icon-folder-open');
+      } else if (this._itemData.image || this._itemData.photo) {
+        _item.find('.odfp-thumbnail .odfp-icon').addClass('icon-file-image');
+      } else if (this._itemData.audio) {
+        _item.find('.odfp-thumbnail .odfp-icon').addClass('icon-file-audio');
+      } else if (this._itemData.video) {
+        _item.find('.odfp-thumbnail .odfp-icon').addClass('icon-file-video');
+      } else {
+        _item.find('.odfp-thumbnail .odfp-icon').addClass('icon-doc-text');
+      }
     }
     if (this._itemData.folder) {
       _item.data('folder', 'true');
